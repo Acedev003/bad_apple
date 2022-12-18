89eca6b27b84a1f7d69a4cf0a845e9e3a1474121
acedev003 <acedeveloper003@gmail.com>
2022-12-19 04:04:08 +0530

    Removed continuous calls to getElement

--- js/badapple.js
+++ js/badapple.js
@@ -1,16 +1,17 @@
 let screen    = document.getElementById("screen");
 let ctrl_disp = document.getElementById("control_disp");
 
-let video  = document.getElementById("video");
+let video  = document.createElement("video");
 let canvas = document.createElement("canvas");
 
 let strt_button = document.getElementById("start_button");
 strt_button.onclick = () =>{
-    ctrl_disp.style.display = "block";
     strt_button.style.display = "none";
 
-    canvas.width  = video.offsetWidth;
-    canvas.height = video.offsetHeight;
+    video.src = "videos/badapple.webm";
+
+    canvas.width  = 100;
+    canvas.height = 75;
 
     playVideo(screen);
 
@@ -20,10 +21,10 @@ strt_button.onclick = () =>{
 let playVideo = (parent) =>{
     parent.style.margin = "10px";
 
-    let vid_w = video.offsetWidth;
-    let vid_h = video.offsetHeight;    
+    let vid_w = 100;
+    let vid_h = 75;    
 
-    
+    let checkboxes = [];    
     for(let i = 0; i < vid_h; i++)
     {
         for(let j = 0; j < vid_w; j++)
@@ -31,6 +32,7 @@ let playVideo = (parent) =>{
             let input  = document.createElement("input");
             input.type = "checkbox";
             parent.appendChild(input);
+            checkboxes.push(input);
         }
         let br = document.createElement("br");
         parent.appendChild(br); 
@@ -54,27 +56,28 @@ let playVideo = (parent) =>{
     };
     
     video.play();
-
+    let count = 0;
     let draw = (now, metadata) => {
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        let data = context.getImageData(0, 0,canvas.width, canvas.height);
-        let pix  = data.data;
-        let text = "";
-
-        for (let i = 0; i < pix.length; i += 4) 
+        if(count%2==0)
         {
-            let greyscale = (pix[i] + pix[i+1] + pix[i+2])/3;
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            let data = context.getImageData(0, 0, canvas.width, canvas.height);
+            let pix = data.data;
+            let text = "";
 
-            if(greyscale < 127.5)
-            {
-                document.getElementsByTagName("input")[i/4].checked = false;
-            }
-            else
-            {
-                document.getElementsByTagName("input")[i/4].checked = true;
-            }
+            for (let i = 0; i < pix.length; i += 4) {
+                let greyscale = (pix[i] + pix[i + 1] + pix[i + 2]) / 3;
 
+                if (greyscale < 127.5) {
+                    checkboxes[i / 4].checked = false;
+                }
+                else {
+                    checkboxes[i / 4].checked = true;
+                }
+
+            }
         }
+        count++;
         video.requestVideoFrameCallback(draw);
     };
 
